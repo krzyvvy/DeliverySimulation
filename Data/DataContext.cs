2798b7f37d02c8f3c1c5a2989bb29e401c42115b
Kacper <kacper.krzywiak88@gmail.com>
2020-04-24 13:58:56 +0200

    First commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using DeliverySimulation.Models.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliverySimulation.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options) : base(options) { }
+
+        public DbSet<Clients> Clients { get; set; }
+    }
+}
---
+++ Migrations/20200424114343_init.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DeliverySimulation.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    ClientId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Full_name = table.Column<string>(nullable: true),
+                    Adress = table.Column<string>(nullable: true),
+                    Country_code = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.ClientId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
