ec56862844b1e7e18ae694c56fcdcca169576421
Kacper <kacper.krzywiak88@gmail.com>
2020-04-26 13:00:26 +0200

    Added Order,OrderHistory controllers

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DeliverySimulation.Models.Information;
+using DeliverySimulation.Service.Order;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,46 @@ namespace DeliverySimulation.Controllers
     [ApiController]
     public class OrderController : ControllerBase
     {
+        private readonly IOrderService _service;
+
+        public OrderController(IOrderService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddOrder(OrderInformation orderInformation)
+        {
+            await _service.AddOrder(orderInformation);
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrders()
+        {
+            var result = await _service.GetOrders();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrder(int id)
+        {
+            var result = await _service.GetOrder(id);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> EditOrder(OrderInformation orderInformation)
+        {
+            await _service.EditOrder(orderInformation);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(int id)
+        {
+            await _service.DeleteOrder(id);
+            return Ok();
+        }
     }
 }
---
+++ Controllers/OrderHistoryController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DeliverySimulation.Models.Information;
+using DeliverySimulation.Service.OrderHistory;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeliverySimulation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderHistoryController : ControllerBase
+    {
+        private readonly IOrderHistoryService _service;
+
+        public OrderHistoryController(IOrderHistoryService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddOrderHistory(OrderHistoryInformation orderHistoryInformation)
+        {
+            await _service.AddOrderHistory(orderHistoryInformation);
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrderHistories()
+        {
+            var result = await _service.GetOrderHistories();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderHistory(int id)
+        {
+            var result = await _service.GetOrderHistory(id);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> EditOrderHistory(OrderHistoryInformation orderHistoryInformation)
+        {
+            await _service.EditOrderHistory(orderHistoryInformation);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrderHistory(int id)
+        {
+            await _service.DeleteOrderHistory(id);
+            return Ok();
+        }
+    }
+}
